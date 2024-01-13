f1959c586db8406529acf8e4c602f836813b083f
fauzaanu <fauzaangasim@gmail.com>
2024-01-13 15:12:26 +0500

    Refactor prompts in DjangoApp to use content from previous steps
    Replaced usage of "purpose" variable in various prompts such as FormsPromptEngine, ViewsPromptEngine, URLPromptEngine etc., with previous file contents to forge a dependency chain. This aims to make the responses more contextual and relevant, as resulting content from each step feeds into the next.

--- AssistedDjango/DjangoApplication.py
+++ AssistedDjango/DjangoApplication.py
@@ -1,7 +1,8 @@
 import os
 import logging
 
-from AssistedDjango.PromptEngine import ModelPromptEngine, FormsPromptEngine, ViewsPromptEngine, URLPromptEngine, TestPromptEngine,AdminPromptEngine
+from AssistedDjango.PromptEngine import ModelPromptEngine, FormsPromptEngine, ViewsPromptEngine, URLPromptEngine, \
+    TestPromptEngine, AdminPromptEngine
 from AssistedDjango.Prompter import Prompter, OpenAISettings
 
 # Configure logging
@@ -12,6 +13,7 @@ class DjangoApplication:
     """
     This class represents a Django application.
     """
+
     def __init__(self, name, purpose, directory):
         self.name = name
         self.purpose = purpose
@@ -39,35 +41,35 @@ class DjangoApplication:
             f.write(models_file_content)
 
         # 2. Create the forms for the models
-        forms_prompt = FormsPromptEngine(self.purpose)
+        forms_prompt = FormsPromptEngine(models_file_content)
         system, prompt = forms_prompt.get_prompt()
         forms_file_content = oai_client.prompt(system, prompt)
         with open(os.path.join(self.directory, 'forms.py'), 'w') as f:
             f.write(forms_file_content)
 
         # 3. Create the views for the forms
-        views_prompt = ViewsPromptEngine(self.purpose)
+        views_prompt = ViewsPromptEngine(models_file_content, self.purpose)
         system, prompt = views_prompt.get_prompt()
         views_file_content = oai_client.prompt(system, prompt)
         with open(os.path.join(self.directory, 'views.py'), 'w') as f:
             f.write(views_file_content)
 
         # 4. Create the urls for the views
-        urls_prompt = URLPromptEngine(self.purpose)
+        urls_prompt = URLPromptEngine(views_file_content)
         system, prompt = urls_prompt.get_prompt()
         urls_file_content = oai_client.prompt(system, prompt)
         with open(os.path.join(self.directory, 'urls.py'), 'w') as f:
             f.write(urls_file_content)
 
         # 5. Create the admin for the models
-        admin_prompt = AdminPromptEngine(self.purpose)
+        admin_prompt = AdminPromptEngine(models_file_content)
         system, prompt = admin_prompt.get_prompt()
         admin_file_content = oai_client.prompt(system, prompt)
         with open(os.path.join(self.directory, 'admin.py'), 'w') as f:
             f.write(admin_file_content)
 
         # 6. Create the tests for the views
-        tests_prompt = TestPromptEngine(self.purpose)
+        tests_prompt = TestPromptEngine(views_file_content)
         system, prompt = tests_prompt.get_prompt()
         tests_file_content = oai_client.prompt(system, prompt)
         with open(os.path.join(self.directory, 'tests.py'), 'w') as f:
